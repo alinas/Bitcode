e4ebd4b74d9c2c1429f5332996287c2a97315e4f
Alina Sbirlea <asbirlea@google.com>
2016-12-15 13:40:14 -0800

    Update Benchmarks folder to test-suite/Bitcode ToT.

--- Benchmarks/Halide/common/halide_image_io.h
+++ Benchmarks/Halide/common/halide_image_io.h
@@ -11,8 +11,6 @@
 #include <string>
 #include <vector>
 
-#include "png.h"
-
 namespace Halide {
 namespace Tools {
 
@@ -105,210 +103,60 @@ struct FileOpener {
     FILE * const f;
 };
 
-struct PngRowPointers {
-    PngRowPointers(int height, int rowbytes) : p(new png_bytep[height]), height(height) {
-        if (p != nullptr) {
-            for (int y = 0; y < height; y++) {
-                p[y] = new png_byte[rowbytes];
-            }
-        }
-    }
-    ~PngRowPointers() {
-        if (p) {
-            for (int y = 0; y < height; y++) {
-                delete[] p[y];
-            }
-            delete[] p;
-        }
-    }
-    png_bytep* const p;
-    int const height;
-};
-
 }  // namespace Internal
 
 
-template<typename ImageType, Internal::CheckFunc check = Internal::CheckReturn>
-bool load_png(const std::string &filename, ImageType *im) {
-    png_byte header[8];
-    png_structp png_ptr;
-    png_infop info_ptr;
+struct BytesImgStruct {
+  int dims[3]; //width, height, channels
+  float* ptr;
+};
 
-    /* open file and test for it being a png */
+
+template<typename ImageType, Internal::CheckFunc check = Internal::CheckReturn>
+bool load_bytes(const std::string &filename, ImageType *im) {
     Internal::FileOpener f(filename.c_str(), "rb");
     if (!check(f.f != nullptr, "File %s could not be opened for reading\n", filename.c_str())) return false;
-    if (!check(fread(header, 1, 8, f.f) == 8, "File ended before end of header\n")) return false;
-    if (!check(!png_sig_cmp(header, 0, 8), "File %s is not recognized as a PNG file\n", filename.c_str())) return false;
 
-    /* initialize stuff */
-    png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    BytesImgStruct ptrStruct;
+    if (!check(fread(ptrStruct.dims, sizeof(int), 3, f.f) == 3,
+               "Could not read dimensions (width, height, channels) for .bytes image\n")) return false;
+    int img_size = ptrStruct.dims[0]*ptrStruct.dims[1];
+    if (!check(ptrStruct.dims[0] > 0 && ptrStruct.dims[1] > 0 && ptrStruct.dims[2] > 0,
+               "File %s does not have valid input\n", filename.c_str())) return false;
+    ptrStruct.ptr = (float*) malloc(img_size * sizeof(float));
+    if (!check(fread(ptrStruct.ptr, sizeof(float), img_size, f.f) == img_size,
+               "Could not read .bytes image\n")) return false;
 
-    if (!check(png_ptr != nullptr, "png_create_read_struct failed\n")) return false;
-
-    info_ptr = png_create_info_struct(png_ptr);
-    if (!check(info_ptr != nullptr, "png_create_info_struct failed\n")) return false;
-
-    if (!check(!setjmp(png_jmpbuf(png_ptr)), "Error during init_io\n")) return false;
-
-    png_init_io(png_ptr, f.f);
-    png_set_sig_bytes(png_ptr, 8);
-
-    png_read_info(png_ptr, info_ptr);
-
-    int width = png_get_image_width(png_ptr, info_ptr);
-    int height = png_get_image_height(png_ptr, info_ptr);
-    int channels = png_get_channels(png_ptr, info_ptr);
-    int bit_depth = png_get_bit_depth(png_ptr, info_ptr);
-
-    // Expand low-bpp images to have only 1 pixel per byte (As opposed to tight packing)
-    if (bit_depth < 8) {
-        png_set_packing(png_ptr);
-    }
-
-    if (channels != 1) {
-        *im = ImageType(width, height, channels);
+    if (ptrStruct.dims[2] != 1) {
+        *im = ImageType(ptrStruct.dims[0], ptrStruct.dims[1], ptrStruct.dims[2]);
     } else {
-        *im = ImageType(width, height);
+        *im = ImageType(ptrStruct.dims[0], ptrStruct.dims[1]);
     }
-
-    png_set_interlace_handling(png_ptr);
-    png_read_update_info(png_ptr, info_ptr);
-
-    // read the file
-    if (!check(!setjmp(png_jmpbuf(png_ptr)), "Error during read_image\n")) return false;
-
-    Internal::PngRowPointers row_pointers(im->height(), png_get_rowbytes(png_ptr, info_ptr));
-    png_read_image(png_ptr, row_pointers.p);
-
-    if (!check((bit_depth == 8) || (bit_depth == 16), "Can only handle 8-bit or 16-bit pngs\n")) return false;
-
-    // convert the data to ImageType::ElemType
-
-    int c_stride = (im->channels() == 1) ? 0 : im->stride(2);
     typename ImageType::ElemType *ptr = (typename ImageType::ElemType*)im->data();
-    if (bit_depth == 8) {
-        for (int y = 0; y < im->height(); y++) {
-            uint8_t *srcPtr = (uint8_t *)(row_pointers.p[y]);
-            for (int x = 0; x < im->width(); x++) {
-                for (int c = 0; c < im->channels(); c++) {
-                    Internal::convert(*srcPtr++, ptr[c*c_stride]);
-                }
-                ptr++;
-            }
-        }
-    } else if (bit_depth == 16) {
-        for (int y = 0; y < im->height(); y++) {
-            uint8_t *srcPtr = (uint8_t *)(row_pointers.p[y]);
-            for (int x = 0; x < im->width(); x++) {
-                for (int c = 0; c < im->channels(); c++) {
-                    uint16_t hi = (*srcPtr++) << 8;
-                    uint16_t lo = hi | (*srcPtr++);
-                    Internal::convert(lo, ptr[c*c_stride]);
-                }
-                ptr++;
-            }
-        }
-    }
-
-    png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-
-    im->set_host_dirty();
+    for (int i=0; i<img_size; i++)
+      ptr[i] = (typename ImageType::ElemType) ptrStruct.ptr[i];
+    free(ptrStruct.ptr);
     return true;
 }
 
-// "im" is not const-ref because copy_to_host() is not const.
 template<typename ImageType, Internal::CheckFunc check = Internal::CheckReturn>
-bool save_png(ImageType &im, const std::string &filename) {
-    png_structp png_ptr;
-    png_infop info_ptr;
-    png_byte color_type;
+bool save_bytes(ImageType &im, const std::string &filename) {
+    BytesImgStruct ptrStruct;
+    ptrStruct.dims[0] = im.width();
+    ptrStruct.dims[1] = im.height();
+    ptrStruct.dims[2] = im.channels();
+    int img_size = ptrStruct.dims[0]*ptrStruct.dims[1];
+    ptrStruct.ptr = (float*) malloc(img_size * sizeof(float));
+    typename ImageType::ElemType *ptr = (typename ImageType::ElemType*)im.data();
+    for (int i=0; i<img_size; i++)
+      ptrStruct.ptr[i] = (float) ptr[i];
 
-    im.copy_to_host();
-
-    if (!check(im.channels() > 0 && im.channels() < 5,
-           "Can't write PNG files that have other than 1, 2, 3, or 4 channels\n")) return false;
-
-    png_byte color_types[4] = {PNG_COLOR_TYPE_GRAY, PNG_COLOR_TYPE_GRAY_ALPHA,
-                               PNG_COLOR_TYPE_RGB,  PNG_COLOR_TYPE_RGB_ALPHA
-                              };
-    color_type = color_types[im.channels() - 1];
-
-    // open file
     Internal::FileOpener f(filename.c_str(), "wb");
-    if (!check(f.f != nullptr, "[write_png_file] File %s could not be opened for writing\n", filename.c_str())) return false;
-
-    // initialize stuff
-    png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-    if (!check(png_ptr != nullptr, "[write_png_file] png_create_write_struct failed\n")) return false;
-
-    info_ptr = png_create_info_struct(png_ptr);
-    if (!check(info_ptr != nullptr, "[write_png_file] png_create_info_struct failed\n")) return false;
-
-    if (!check(!setjmp(png_jmpbuf(png_ptr)), "[write_png_file] Error during init_io\n")) return false;
-
-    png_init_io(png_ptr, f.f);
-
-    unsigned int bit_depth = 16;
-    if (sizeof(typename ImageType::ElemType) == 1) {
-        bit_depth = 8;
-    }
-
-    // write header
-    if (!check(!setjmp(png_jmpbuf(png_ptr)), "[write_png_file] Error during writing header\n")) return false;
-
-    png_set_IHDR(png_ptr, info_ptr, im.width(), im.height(),
-                 bit_depth, color_type, PNG_INTERLACE_NONE,
-                 PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
-
-    png_write_info(png_ptr, info_ptr);
-
-    Internal::PngRowPointers row_pointers(im.height(), png_get_rowbytes(png_ptr, info_ptr));
-
-    // im.copyToHost(); // in case the image is on the gpu
-
-    int c_stride = (im.channels() == 1) ? 0 : im.stride(2);
-    typename ImageType::ElemType *srcPtr = (typename ImageType::ElemType*)im.data();
-
-    for (int y = 0; y < im.height(); y++) {
-        uint8_t *dstPtr = (uint8_t *)(row_pointers.p[y]);
-        if (bit_depth == 16) {
-            // convert to uint16_t
-            for (int x = 0; x < im.width(); x++) {
-                for (int c = 0; c < im.channels(); c++) {
-                    uint16_t out;
-                    Internal::convert(srcPtr[c*c_stride], out);
-                    *dstPtr++ = out >> 8;
-                    *dstPtr++ = out & 0xff;
-                }
-                srcPtr++;
-            }
-        } else if (bit_depth == 8) {
-            // convert to uint8_t
-            for (int x = 0; x < im.width(); x++) {
-                for (int c = 0; c < im.channels(); c++) {
-                    uint8_t out;
-                    Internal::convert(srcPtr[c*c_stride], out);
-                    *dstPtr++ = out;
-                }
-                srcPtr++;
-            }
-        } else {
-            if (!check(bit_depth == 8 || bit_depth == 16, "We only support saving 8- and 16-bit images.")) return false;
-        }
-    }
-
-    // write data
-    if (!check(!setjmp(png_jmpbuf(png_ptr)), "[write_png_file] Error during writing bytes")) return false;
-
-    png_write_image(png_ptr, row_pointers.p);
-
-    // finish write
-    if (!check(!setjmp(png_jmpbuf(png_ptr)), "[write_png_file] Error during end of write")) return false;
-
-    png_write_end(png_ptr, NULL);
-
-    png_destroy_write_struct(&png_ptr, &info_ptr);
-
+    if (!check(f.f != nullptr, "File %s could not be opened for writing\n", filename.c_str())) return false;
+    if (!check(fwrite (ptrStruct.dims, sizeof(int), 3, f.f),
+               "Could not write dimensions (width, height, channels) for .bytes image\n")) return false;
+    if (!check(fwrite (ptrStruct.ptr, sizeof(float), img_size, f.f),
+               "Could not write .bytes image\n")) return false;
     return true;
 }
 
@@ -374,7 +222,7 @@ template<typename ImageType, Internal::CheckFunc check = Internal::CheckReturn>
 bool save_ppm(ImageType &im, const std::string &filename) {
     im.copy_to_host();
 
-	unsigned int bit_depth = sizeof(typename ImageType::ElemType) == 1 ? 8: 16;
+    unsigned int bit_depth = sizeof(typename ImageType::ElemType) == 1 ? 8: 16;
 
     Internal::FileOpener f(filename.c_str(), "wb");
     if (!check(f.f != nullptr, "File %s could not be opened for writing\n", filename.c_str())) return false;
@@ -416,24 +264,24 @@ bool save_ppm(ImageType &im, const std::string &filename) {
 // Returns false upon failure.
 template<typename ImageType, Internal::CheckFunc check = Internal::CheckReturn>
 bool load(const std::string &filename, ImageType *im) {
-    if (Internal::ends_with_ignore_case(filename, ".png")) {
-        return load_png<ImageType, check>(filename, im);
-    } else if (Internal::ends_with_ignore_case(filename, ".ppm")) {
+    if (Internal::ends_with_ignore_case(filename, ".ppm")) {
         return load_ppm<ImageType, check>(filename, im);
+    } else if (Internal::ends_with_ignore_case(filename, ".bytes")) {
+        return (load_bytes<ImageType, check>(filename, im));
     } else {
-        return check(false, "[load] unsupported file extension (png|ppm supported)");
+        return check(false, "[load] unsupported file extension (bytes|ppm supported)");
     }
 }
 
 // Returns false upon failure.
 template<typename ImageType, Internal::CheckFunc check = Internal::CheckReturn>
 bool save(ImageType &im, const std::string &filename) {
-    if (Internal::ends_with_ignore_case(filename, ".png")) {
-        return save_png<ImageType, check>(im, filename);
-    } else if (Internal::ends_with_ignore_case(filename, ".ppm")) {
+    if (Internal::ends_with_ignore_case(filename, ".ppm")) {
         return save_ppm<ImageType, check>(im, filename);
+    } else if (Internal::ends_with_ignore_case(filename, ".bytes")) {
+        return save_bytes<ImageType, check>(im, filename);
     } else {
-        return check(false, "[save] unsupported file extension (png|ppm supported)");
+        return check(false, "[save] unsupported file extension (bytes|ppm supported)");
     }
 }
 
